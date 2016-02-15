28311563885be77e7818ef3481f0f757218162cb
Andrew <06frosta@gmail.com>
2016-02-15 14:01:54 +0000

    I do need the Opengl hints, The issues is setting it to 3.3 causes a crash on machines with older OpenGL

--- mopViewer/window.cpp
+++ mopViewer/window.cpp
@@ -34,11 +34,11 @@ void testWindow::init ( void )
 
 	glfwInit();                                                         //Init glfw
 	//For purposes of learning get OpenGL version to 3.3
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	//Using core profile of OpenGL prevents use of any legacy functions
-	//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);                                                         //Stop the window from being resized
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);                                                         //Stop the window from being resized
 	//Create a glfw window, size 800 x 600
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {                                                         //Some error checking
