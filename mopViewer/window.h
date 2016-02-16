5cc2f106e77e149b98e28f53ee42507de1751a82
Andrew Frost <06frosta@gmail.com>
2016-02-16 21:41:01 +0000

    Add option to change mopfile

--- mopViewer/window.h
+++ mopViewer/window.h
@@ -7,13 +7,14 @@
 #include <iostream>
 #include "../mopLoader/mopfile/MopFile.h"
 class testWindow {
-public:
-	testWindow(void);
-	~testWindow(void);
-	void init(std::string fileName, int skipCount);
-	static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-	GLfloat deltaTime;	// Time between current frame and last frame
-	GLfloat lastFrame;
-	MopFile * mopfile;
-	MopState * mopstate;
+ public:
+  testWindow(void);
+  ~testWindow(void);
+  void init(std::string fileName, int skipCount);
+  static void key_callback(GLFWwindow* window, int key, int scancode,
+                           int action, int mode);
+  GLfloat deltaTime;  // Time between current frame and last frame
+  GLfloat lastFrame;
+  MopFile* mopfile;
+  MopState* mopstate;
 };
